3f14e3aea780339cb6c9776e8445282d9d562bd9
Ez-21 <80458642+Ez-21@users.noreply.github.com>
2021-12-28 20:25:46 +0800

    2021年12月28日20:25:44

---
+++ src/pages/MessageList/messageList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function MessageList(props){
+    return(
+        <div>
+            <div className='MessageBack'>《</div>
+            我是消息列表
+        </div>
+    )
+}
+export default MessageList
