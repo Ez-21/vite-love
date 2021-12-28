0982b6ebb427f2c0d64d893b24c91b8012217e92
Ez-21 <80458642+Ez-21@users.noreply.github.com>
2021-12-28 21:44:07 +0800

    2021年12月28日21:44:04

--- src/pages/MoveContent/MoveContent.jsx
+++ src/pages/MoveContent/MoveContent.jsx
@@ -3,33 +3,34 @@ import { useParams } from "react-router-dom";
 import { MovContent } from "/src/api/home";
 import { message } from "antd";
 import smile from "/src/pages/img/yellow7.png";
-
+import '../Css/MoveContent.scss'
 // 组件
 function Content() {
-	const params = useParams();
-	console.log('params: ', params);
-	const [mess, SetMess] = useState({});
-	// 获取电影详情数据
-	function MovCon(id) {
-		MovContent({ movieId: Number(id) })
-			.then((res) => {
-				if(res.code!=200){
-					message.error(res.msg)
-				}else{
-					SetMess(res.detailMovie);
-				}
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}
-	useEffect(()=>MovCon(params.params))
-	return (
-		<div className="MoveContent">
-			<span>
-				<video src={""}></video>
-			</span>
+  const params = useParams();
+  console.log("params: ", params);
+  const [mess, SetMess] = useState({});
+  // 获取电影详情数据
+  function MovCon(id) {
+    MovContent({ movieId: Number(id) })
+      .then((res) => {
+        SetMess(res.detailMovie);
+		console.log(mess)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  useEffect(() => MovCon(params.params),[]);
+  return (
+    <div className="MoveContent">
+		<div className='MoveHead'>
+			<p>{mess.nm}</p>
+			<p>{mess.oriLang}</p>
+			<p>{mess.pubDesc}</p>
+			<p>{mess.scoreLabel}：{mess.sc}</p>
 		</div>
-	);
+        <video src={mess.videourl} controls poster={mess.videoImg}></video>
+    </div>
+  );
 }
 export default Content;
