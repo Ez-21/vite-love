d2c295957dcb71a5a96bb7a790e211a2e94e3b1f
Qzz <2275862144@qq.com>
2021-12-28 13:21:21 +0800

    2021年12月28日13:21:17

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { useState } from "react";
+import { useState,useEffect} from "react";
 import { Button } from "antd";
 import { ReactDOM, React } from "../global/global";
 import { Route, Link, Routes, useNavigate,Outlet,NavLink} from "react-router-dom";
 import "antd/dist/antd.css";
 import '../src/pages/Css/App.scss'
+import path from "path";
+
 function App() {
+	const Path = [
+		{id:1,pagePath:'/Home',imgSrc:'home'},
+		{id:2,pagePath:'/Shop',imgSrc:'star'},
+		{id:3,pagePath:'/Mine',imgSrc:'mine'},
+	]
+	const getImageUrl = (name) => {
+		return new URL(`pages/img/${name}.png`, import.meta.url).href
+	}
+	const Gopath = (item)=>{
+			item.imgSrc = item.imgSrc+'ed'
+	}
 	return (
 		<div>
 			<Outlet></Outlet>
 			<div className="Tar">
-			<NavLink to="/Home">首页</NavLink>
-			<NavLink to="/Shop">推荐</NavLink>
-			<NavLink to="/Mine">我的</NavLink>
+				{
+					Path.map(item=>{
+						return(
+							<NavLink to={item.pagePath} key={item.id} onClick={()=>Gopath(item)}>
+							<img src={getImageUrl(item.imgSrc)} alt="" />
+						</NavLink>
+						)
+					})
+				}
+			
 			</div>
 		</div>
 	);
--- src/pages/Mine/mine.jsx
+++ src/pages/Mine/mine.jsx
@@ -2,13 +2,12 @@ import react from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../pages/Css/Mine.scss";
 const List = [
-	{ name: "我的余额", id: 1 },
-	{ name: "我的消息", id: 2 },
-	{ name: "浏览历史", id: 3 },
-	{ name: "关于我们", id: 4 },
-	{ name: "退出登录", id: 5 },
+	{ name: "我的余额", id: 1 ,imgUrl:'money'},
+	{ name: "我的消息", id: 2 ,imgUrl:'message'},
+	{ name: "浏览历史", id: 3 ,imgUrl:'calendar'},
+	{ name: "护眼模式", id: 4 ,imgUrl:'theme'},
+	{ name: "退出登录", id: 5 ,imgUrl:'user'},
 ];
-import smile from "../img/yellow1.png";
 function Jon(index,Go){
 	switch (index) {
 		case 4:{Go('/Login'); break;}
@@ -17,6 +16,9 @@ function Jon(index,Go){
 }
 // 接收路由传递参数
 export default function () {
+	const getImageUrl = (name) => {
+		return new URL(`../img/${name}.png`, import.meta.url).href
+	}
 	const Nav = useNavigate();
 	return (
 		<div className="box">
@@ -31,7 +33,7 @@ export default function () {
 			<div className="MineList">
 				{List.map((item,index) => (
 					<div key={item.id} className="ListBox" onClick={()=>Jon(index,Nav)}>
-						<img src="https://p9-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/ac95b0b87cf64096a78bdacc9e811dc8~tplv-k3u1fbpfcp-no-mark:100:100:100:100.awebp?" />
+						<img src={getImageUrl(item.imgUrl)} alt="" />
 						{item.name}
 					</div>
 				))}
